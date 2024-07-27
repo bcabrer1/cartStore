2477b01debc61e4f356a81c7a67a1d6f6b10c9c9
Ben <bcabrer1@terpmail.umd.edu>
2024-07-27 18:07:58 -0400

    adding cart features.

--- src/main.js
+++ src/main.js
@@ -37,17 +37,21 @@ let generateShop = () => {
         .map((x) => {
 
 let { id, name, price, desc, img } = x;
+
+let search = basket.find(() => x.id === id) || []
     return `
     <div id=product-id-${id}class="item">
     <img width= "220" src=${img} alt="">
     <div class="details">
-        <h3> ${name}</h3>
-        <p> ${desc} </p>
+        <h3>${name}</h3>
+        <p>${desc}</p>
         <div class="price-quantity">
          <h2>${price}</h2>
          <div class="buttons">
             <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-            <div id=${id} class="quantity">0</div>
+            <div id=${id} class="quantity"> 
+            ${search.item === undefined? 0: search.item}
+            </div>
             <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
 
          </div>   
@@ -79,19 +83,25 @@ let increment = (id) => {
     // this is the ket when sotring data
     localStorage.setItem("data", JSON.stringify(basket));
     update(selectedItem.id);
+
+    localStorage.setItem("data", JSON.stringify(basket));
+
 };
 let decrement = (id) => {
     let selectedItem = id;
     let search = basket.find((x) => x.id === selectedItem.id);
 
-    if (search.item === 0) return;
+    if (search === undefined) return;
+    else if (search.item === 0) return;
      else {
         search.item -=1;
     }
+    update(selectedItem.id);
 
+    basket = basket.filter((x)=>x.item !== 0)
+  
     localStorage.setItem("data", JSON.stringify(basket));
 
-    update(selectedItem.id);
 }
 let update = (id) => {
     let search = basket.find((x) => x.id === id);
@@ -104,3 +114,5 @@ let calculation = () => {
     let cartIcon = document.getElementById("cartAmount");
     cartIcon.innerHTML = basket.map((x) => x.item).reduce((x,y) => x + y, 0);
 };
+
+calculation();
